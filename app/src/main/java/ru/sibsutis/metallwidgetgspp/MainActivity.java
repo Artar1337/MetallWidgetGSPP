d548bd8a43a7547c5781480c1c43cb11a2f42646
Artem <armastra@gmail.com>
2021-03-01 14:32:22 +0600

    Created selector for table using Jsoup

--- app/src/main/java/ru/sibsutis/metallwidgetgspp/MainActivity.java
+++ app/src/main/java/ru/sibsutis/metallwidgetgspp/MainActivity.java
@@ -22,46 +22,59 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String [] doInBackground(Void... params) {
-            String result [] = {" "," "," "," "};
+            // дата и 4 цены
+            String result [] = {" "," "," "," "," "};
             String url = "https://www.cbr.ru/hd_base/metall/metall_base_new/";
             Document doc = null;
             try {
                 doc = Jsoup.connect(url).get();
-                String title = doc.title();
-                result[0]=title.substring(0,50);
-                Log.wtf(" ",result[0]);
+                //нужная информация находится в body->div.table->table.data->строка №2
+                Elements title = doc.body().
+                        select("table.data").first()
+                        .select("tr").get(1).select("td");
+
+                for(int i=0;i<5;i++)
+                result[i]=title.get(i).text();
+
             } catch (IOException e) {
                 e.printStackTrace();
+                for(int i=0;i<5;i++)
+                    result[i]="ERROR";
             }
             return result;
         }
         @Override
         protected void onPostExecute(String price [])
         {
-            new ParseTask().execute();;
-
             TableLayout table = findViewById(R.id.MainTable);
+            TextView header = findViewById(R.id.Header);
             table.setShrinkAllColumns(true);
             table.setStretchAllColumns(true);
 
             String value = getString(R.string.value);
+            String head = getString(R.string.header) + " " + price[0];
 
-            addLine(table,getString(R.string.goldannotation),price[0]+" "+value);
-            addLine(table,getString(R.string.silvannotation),price[1]+" "+value);
-            addLine(table,getString(R.string.platannotation),price[2]+" "+value);
-            addLine(table,getString(R.string.pallannotation),price[3]+" "+value);
+            header.setText(head);
+            addLine(table,getString(R.string.goldannotation),price[1]+" "+value);
+            addLine(table,getString(R.string.silvannotation),price[2]+" "+value);
+            addLine(table,getString(R.string.platannotation),price[3]+" "+value);
+            addLine(table,getString(R.string.pallannotation),price[4]+" "+value);
         }
     }
 
-        private void addLine(TableLayout tableLayout,String prefix, String postfix)
+    private void addLine(TableLayout tableLayout,String prefix, String postfix)
     {
+        //создаем строку
+        // добавляем название металла
         TableRow row = new TableRow(this);
         TextView textview = new TextView(this);
         textview.setTextColor(getResources().getColor(R.color.black));
         textview.setBackgroundColor(getResources().getColor(R.color.tablebackground));
         textview.setText(prefix);
+
         row.addView(textview);
 
+        //добавляем цену металла
         TextView textview1 = new TextView(this);
         textview1.setTextColor(getResources().getColor(R.color.black));
         textview1.setBackgroundColor(getResources().getColor(R.color.tablebackground));
@@ -78,7 +91,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void tableUpdate()
     {
-        new ParseTask().execute();;
+        //выполняем запрос в фоне
+        new ParseTask().execute();
     }
 
     @Override
