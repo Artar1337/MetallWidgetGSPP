c04e0c82db9d455cbac2d118a75d57c72f788daf
Artem <armastra@gmail.com>
2021-03-01 14:11:51 +0600

    Handled android.os.NetworkOnMainThreadException

--- app/src/main/java/ru/sibsutis/metallwidgetgspp/MainActivity.java
+++ app/src/main/java/ru/sibsutis/metallwidgetgspp/MainActivity.java
@@ -2,7 +2,9 @@ package ru.sibsutis.metallwidgetgspp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -16,7 +18,42 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private void addLine(TableLayout tableLayout,String prefix, String postfix)
+    private class ParseTask extends AsyncTask<Void, Void, String []> {
+
+        @Override
+        protected String [] doInBackground(Void... params) {
+            String result [] = {" "," "," "," "};
+            String url = "https://www.cbr.ru/hd_base/metall/metall_base_new/";
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(url).get();
+                String title = doc.title();
+                result[0]=title.substring(0,50);
+                Log.wtf(" ",result[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+        @Override
+        protected void onPostExecute(String price [])
+        {
+            new ParseTask().execute();;
+
+            TableLayout table = findViewById(R.id.MainTable);
+            table.setShrinkAllColumns(true);
+            table.setStretchAllColumns(true);
+
+            String value = getString(R.string.value);
+
+            addLine(table,getString(R.string.goldannotation),price[0]+" "+value);
+            addLine(table,getString(R.string.silvannotation),price[1]+" "+value);
+            addLine(table,getString(R.string.platannotation),price[2]+" "+value);
+            addLine(table,getString(R.string.pallannotation),price[3]+" "+value);
+        }
+    }
+
+        private void addLine(TableLayout tableLayout,String prefix, String postfix)
     {
         TableRow row = new TableRow(this);
         TextView textview = new TextView(this);
@@ -34,36 +71,14 @@ public class MainActivity extends AppCompatActivity {
         tableLayout.addView(row);
     }
 
-    private void valuesUpdate(String value []){
-        String url = "https://www.cbr.ru/hd_base/metall/metall_base_new/";
-        Document doc = null;
-        try {
-            doc = Jsoup.connect(url).get();
-            String title = doc.title();
-            value[0]=title.substring(0,50);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+    private String [] valuesUpdate(){
+        ParseTask task = new ParseTask();
+        return task.doInBackground();
     }
 
     private void tableUpdate()
     {
-        String  price [] = {"1","2","3","4"};
-        valuesUpdate(price);
-
-        TableLayout table = findViewById(R.id.MainTable);
-        table.setShrinkAllColumns(true);
-        table.setStretchAllColumns(true);
-
-        String value = getString(R.string.value);
-
-        addLine(table,getString(R.string.goldannotation),price[0]+" "+value);
-        addLine(table,getString(R.string.silvannotation),price[1]+" "+value);
-        addLine(table,getString(R.string.platannotation),price[2]+" "+value);
-        addLine(table,getString(R.string.pallannotation),price[3]+" "+value);
-
+        new ParseTask().execute();;
     }
 
     @Override
